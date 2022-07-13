d3ca410173d64f99ed2ae9820103f8187bdc2ca5
Bryan Davis <bd808@wikimedia.org>
2022-07-13 14:15:35 -0600

    Add some tests!
    Add basic tests for our ServiceWiring and Hooks content.
    
    Change-Id: Ia6d390eb4f54ea1bde8bcdfe44e71712a8ae6602

---
+++ tests/phpunit/integration/ServiceWiringTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @section LICENSE
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki\Extension\Toolhub\Tests\Integration;
+
+use MediaWiki\Extension\Toolhub\ApiClient;
+use MediaWiki\MediaWikiServices;
+use MediaWikiIntegrationTestCase;
+
+/**
+ *
+ * @copyright © 2022 Wikimedia Foundation and contributors
+ * @coversNothing
+ */
+class ServiceWiringTest extends MediaWikiIntegrationTestCase {
+
+	public function testServices() {
+		$apiClient = MediaWikiServices::getInstance()
+			->getService( 'Toolhub.ApiClient' );
+		$this->assertInstanceOf( ApiClient::class, $apiClient );
+	}
+
+}
--- tests/phpunit/unit/HooksTest.php
+++ tests/phpunit/unit/HooksTest.php
@@ -2,11 +2,29 @@
 
 namespace MediaWiki\Extension\Toolhub\Tests;
 
+use MediaWiki\Extension\Toolhub\CoreLibrary;
 use MediaWiki\Extension\Toolhub\Hooks;
+use MediaWikiUnitTestCase;
 
 /**
- * @coversDefaultClass \MediaWiki\Extension\Toolhub\Hooks
+ * @covers \MediaWiki\Extension\Toolhub\Hooks
  */
-class HooksTest extends \MediaWikiUnitTestCase {
+class HooksTest extends MediaWikiUnitTestCase {
+
+	public function testOnScribuntoExternalLibraries() {
+		$hooks = new Hooks();
+		$extraLibraries = [];
+
+		$hooks->onScribuntoExternalLibraries( 'not-lua', $extraLibraries );
+		$this->assertArrayEquals( [], $extraLibraries );
+
+		$hooks->onScribuntoExternalLibraries( 'lua', $extraLibraries );
+		$this->assertArrayHasKey( 'mw.ext.toolhub', $extraLibraries );
+		$lib = $extraLibraries[ 'mw.ext.toolhub' ];
+		$this->assertArrayHasKey( 'class', $lib );
+		$this->assertArrayHasKey( 'deferLoad', $lib );
+		$this->assertEquals( CoreLibrary::class, $lib[ 'class' ] );
+		$this->assertTrue( $lib[ 'deferLoad' ] );
+	}
 
 }
